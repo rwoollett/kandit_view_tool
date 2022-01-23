9708fd521d53519cf47d9b09f4f7c33090eb48a1
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-23 16:49:23 +1300

    Kandit tool initial commit

---
+++ lib/kandit_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "kandit_view_tool/version"
+require_relative "kandit_view_tool/renderer"
+
+module KanditViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kandit_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KanditViewTool
+  VERSION = "0.1.0"
+end
